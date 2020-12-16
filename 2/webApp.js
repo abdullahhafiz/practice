518bf7d7151da304c8077e5fe4df5066454732e5
abdullah.123 <abdullahhafiz1997@gmail.com>
2020-12-16 11:59:48 +0500

    newChange

--- 2/webApp.js
+++ 2/webApp.js
@@ -1,15 +1,15 @@
     const express = require('express');
     const webApp = express();
-    const port = 8080;
+    
     webApp.get('/' ,(req, res)=>{
         res.send('Hello World');
     }).listen(8080);
 
+    // const port = 8080;
     /* webApp.listen(port,()=>{
         console.log('Web App Is Listening ')
     });*/
-    console.log('Hello World');
-    console.log('');
+    
 
 
     
